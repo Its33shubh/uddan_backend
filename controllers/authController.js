210a32333ddf9b64c938960103e7ba3310d5b341
Its33shubh <shubhamkakloter123@gmail.com>
2026-05-08 16:12:03 +0530

    add register and login api

---
+++ controllers/authController.js
@@ -0,0 +1,108 @@
+const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+// REGISTER
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, phone, password, role } = req.body;
+
+        if (!name || !email || !phone || !password) {
+            return res.status(400).json({
+                success: false,
+                error: true,
+                message: "All fields are required"
+            });
+        }
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({
+                success: false,
+                error: true,
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            phone,
+            password: hashedPassword,
+            role: role || "rider"
+        });
+
+        res.status(201).json({
+            success: true,
+            error: false,
+            message: "User registered successfully",
+            data: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: true,
+            message: error.message
+        });
+    }
+};
+
+
+
+// // LOGIN
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "User not found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+module.exports = {registerUser,loginUser};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require('./config/db')
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Taxi Booking Backend Running...");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
