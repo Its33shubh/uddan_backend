da5f8cfe21754c4caf88b582cbd5851da9f8ffd6
Its33shubh <shubhamkakloter123@gmail.com>
2026-05-15 11:40:33 +0530

    setup the socket.io for real time app working

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require('./config/db')
+const http = require("http");
+const { Server } = require("socket.io");
+
+const connectDB = require("./config/db");
+
 const riderRoutes = require("./routes/riderRoutes");
 const driverRoutes = require("./routes/driverRoutes");
-const adminRoutes = require("./routes/adminRoutes")
+const adminRoutes = require("./routes/adminRoutes");
 const rideRoutes = require("./routes/rideRoutes");
+
 dotenv.config();
 
-connectDB()
+connectDB();
 
 const app = express();
 
@@ -16,16 +21,52 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Taxi Booking Backend Running...");
+  res.send("Taxi Booking Backend Running...");
 });
 
 app.use("/api/rider", riderRoutes);
 app.use("/api/driver", driverRoutes);
-app.use("/api/admin",adminRoutes)
+app.use("/api/admin", adminRoutes);
 app.use("/api/rides", rideRoutes);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PATCH"]
+    }
+  });
+  
+  const onlineDrivers = {};
+  
+  io.on("connection", (socket) => {
+    console.log("Socket Connected:", socket.id);
+  
+    socket.on("driver_online", (data) => {
+      const { driverId } = data;
+  
+      onlineDrivers[driverId] = socket.id;
+  
+      console.log("Online Drivers:", onlineDrivers);
+    });
+  
+    socket.on("disconnect", () => {
+      for (let driverId in onlineDrivers) {
+        if (onlineDrivers[driverId] === socket.id) {
+          delete onlineDrivers[driverId];
+        }
+      }
+  
+      console.log("Socket Disconnected:", socket.id);
+      console.log("Online Drivers:", onlineDrivers);
+    });
+  });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
+
+module.exports = { io, onlineDrivers };
