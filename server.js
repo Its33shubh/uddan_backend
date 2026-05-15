12403a9d2d5bdc2fd595049364c6e020617db933
Its33shubh <shubhamkakloter123@gmail.com>
2026-05-15 15:24:59 +0530

    add the api for accept request

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const riderRoutes = require("./routes/riderRoutes");
 const driverRoutes = require("./routes/driverRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const rideRoutes = require("./routes/rideRoutes");
+const Ride = require("./models/Ride");
 
 dotenv.config();
 
@@ -68,7 +69,40 @@ io.on("connection", (socket) => {
       
         console.log("Online Riders:", onlineRiders);
       });
-  
+      socket.on("accept_ride", async (data) => {
+        try {
+          const parsedData =
+            typeof data === "string" ? JSON.parse(data) : data;
+      
+          const { rideId, driverId } = parsedData;
+      
+          const ride = await Ride.findById(rideId);
+      
+          if (!ride) {
+            console.log("Ride not found");
+            return;
+          }
+      
+          ride.driverId = driverId;
+          ride.rideStatus = "accepted";
+      
+          await ride.save();
+      
+          const riderSocketId = onlineRiders[ride.riderId.toString()];
+      
+          if (riderSocketId) {
+            io.to(riderSocketId).emit("ride_accepted", {
+              message: "Driver accepted your ride",
+              ride
+            });
+          }
+      
+          console.log("Ride accepted:", ride._id);
+      
+        } catch (error) {
+          console.log(error.message);
+        }
+      });
     socket.on("disconnect", () => {
       // remove drivers
       for (let driverId in onlineDrivers) {
